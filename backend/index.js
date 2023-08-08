5bde9a79b18f01268115c2fea6839c368df936cc
Harikanth <harikanth664@gmail.com>
2023-08-08 19:10:28 +0530

    Video 44 creating models Schema

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,10 @@ const express = require('express');
 const app = express();
 const port = 3001
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+
+// Available Routs
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
