930b54f021430b5da078fdc283771535e52b11d3
Harikanth <harikanth664@gmail.com>
2023-08-08 22:27:29 +0530

    Video 47 collections error handling with validation

--- backend/models/User.js
+++ backend/models/User.js
@@ -21,4 +21,6 @@ const UserSchema = new Schema({
     }
   });
 
-  module.exports = mongoose.model('user',UserSchema);
+  const User = mongoose.model('user',UserSchema);
+  // To restrict duplicate entries
+  module.exports = User
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,39 @@
 const express = require('express');
-const User =require('../models/User');
+const User = require('../models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
+// Create a User using: POST "/api/auth/createuser". No login requied
+router.post('/createuser', [
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must be atleast 5 charecters').isLength({ min: 5 })
 
-// Create a User using: POST "/api/auth/". Doesn't require authentication
-router.post('/', (req,res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save()
-    res.send(req.body)
+], async (req, res) => {
+
+    // If errors not null return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Check weather the user with this email exsists already
+    let user = await User.findOne({ email: req.body.email});
+    if (user) {
+        return res.status(400).json({ error: "Sorry a user with this email already exists"})
+    }
+
+    // Create new user in DB
+    user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    })
+
+    // .then(user => res.json(user))
+    // .catch(err=> {console.log(err)
+    //     res.json({ error: 'Please enter a unique value for email', message: err.message})})
+    res.json(user);
 })
 
 module.exports = router 
